a85926d6c9e80b3abd0feb01f6d6a7274d851915
Kevin Chiu <kschiu@andrew.cmu.edu>
2015-09-15 16:43:24 -0400

    Added migration to include ID for event

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150912220349) do
+ActiveRecord::Schema.define(version: 20150915204228) do
 
   create_table "events", force: true do |t|
     t.string   "status"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20150912220349) do
     t.string   "recurrence"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "google_id"
   end
 
   create_table "tasks", force: true do |t|
