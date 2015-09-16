a45dc39fa11b1b68e051eb075d77e5fae11173d1
Serena Chen <serenasyue@gmail.com>
2015-09-16 00:11:20 -0400

    Add new task ajax.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,20 +1,24 @@
-class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
+class TasksController < ApplicationController
+  # before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :all_tasks, only: [:index, :edit, :create, :update, :destroy]
+  before_action :set_tasks, only: [:edit, :update, :destroy]
+  respond_to :html, :js
   # GET /tasks
   # GET /tasks.json
-  def index
-    @tasks = Task.all
-  end
+  # def index
+  #   @tasks = Task.all
+  # end
 
   # GET /tasks/1
   # GET /tasks/1.json
-  def show
-  end
+  # def show
+  # end
 
   # GET /tasks/new
   def new
     @task = Task.new
+    @task.event_id = params[:id] unless params[:id].nil?
   end
 
   # GET /tasks/1/edit
@@ -23,7 +27,7 @@ class TasksController < ApplicationController
       if params[:status] == 'completed'
         @task.update_attribute(:is_complete, true)
         @task.save!
-      else
+      elsif params[:status] == 'incomplete'
         @task.update_attribute(:is_complete, false)
         @task.save!
       end
@@ -35,16 +39,16 @@ class TasksController < ApplicationController
   # POST /tasks.json
   def create
     @task = Task.new(task_params)
-
-    respond_to do |format|
-      if @task.save
-        format.html { redirect_to @task, notice: 'Task was successfully created.' }
-        format.json { render :show, status: :created, location: @task }
-      else
-        format.html { render :new }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
-    end
+    @task.save!
+    # respond_to do |format|
+    #   if @task.save
+    #     format.html { redirect_to @task, notice: 'Task was successfully created.' }
+    #     format.json { render :show, status: :created, location: @task }
+    #   else
+    #     format.html { render :new }
+    #     format.json { render json: @task.errors, status: :unprocessable_entity }
+    #   end
+    # end
   end
 
   # PATCH/PUT /tasks/1
@@ -56,20 +60,26 @@ class TasksController < ApplicationController
       render :action => 'edit'
     end
   end
-
+  # def update
+  #   @task.update_attributes(task_params)
+  # end
   # DELETE /tasks/1
   # DELETE /tasks/1.json
   def destroy
     @task.destroy
-    respond_to do |format|
-      format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    # respond_to do |format|
+    #   format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
+    #   format.json { head :no_content }
+    # end
   end
 
   private
+    def all_tasks
+      @tasks = Task.all
+    end
+
     # Use callbacks to share common setup or constraints between actions.
-    def set_task
+    def set_tasks
       @task = Task.find(params[:id])
     end
 
