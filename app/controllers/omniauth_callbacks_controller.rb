bd22e5a6a1c8d7e141bcf735ac77a68193463d45
Kevin Chiu <kschiu@andrew.cmu.edu>
2015-09-16 00:02:28 -0400

    Added check to create events only when new user

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -4,7 +4,9 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       @user = User.from_omniauth(request.env["omniauth.auth"])
 
       if @user.persisted?
-        import_events(request.env["omniauth.auth"])
+        if @user.sign_in_count == 0
+          import_events(request.env["omniauth.auth"])
+        end
         flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
         redirect_to tasks_url
       else
